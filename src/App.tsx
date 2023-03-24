0f7df77adb4c41b05073e7474a5026431b7acb20
MUHAMMAD ILYAS <mail.ilyas@yahoo.com>
2023-03-24 14:25:19 +0500

    react native paper added

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,14 @@ import { PersistGate } from 'redux-persist/lib/integration/react';
 import { store, persistor } from './store';
 import ApplicationNavigator from './navigators/Application';
 import './translations';
+import { Provider as PaperProvider } from 'react-native-paper';
 
 const App = () => (
   <Provider store={store}>
-    {/**
-     * PersistGate delays the rendering of the app's UI until the persisted state has been retrieved
-     * and saved to redux.
-     * The `loading` prop can be `null` or any react instance to show during loading (e.g. a splash screen),
-     * for example `loading={<SplashScreen />}`.
-     * @see https://github.com/rt2zz/redux-persist/blob/master/docs/PersistGate.md
-     */}
     <PersistGate loading={null} persistor={persistor}>
-      <ApplicationNavigator />
+      <PaperProvider>
+        <ApplicationNavigator />
+      </PaperProvider>
     </PersistGate>
   </Provider>
 );
