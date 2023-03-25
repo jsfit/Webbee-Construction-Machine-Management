30c55372207613bff021819e3b55340c8d4ce535
MUHAMMAD ILYAS <mail.ilyas@yahoo.com>
2023-03-25 07:52:23 +0500

    item model seprated from category modal

--- src/models/CategoryModel.ts
+++ src/models/CategoryModel.ts
@@ -1,14 +1,9 @@
-import {
-  makeAutoObservable,
-  autorun,
-  toJS,
-  makeObservable,
-  observable,
-} from 'mobx';
+import { makeAutoObservable } from 'mobx';
 import { FieldModel, IField } from './Fields';
 import { v4 as uuidv4 } from 'uuid';
 import { makePersistable } from 'mobx-persist-store';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Item } from './ItemModal';
 
 export interface ICategory {
   _id: string;
@@ -19,41 +14,6 @@ export interface ICategory {
 export interface ICategoryList {
   _id: string;
   categories: CategoryModel[];
-}
-
-export class Item {
-  _id: string = '';
-  model = observable.object({});
-  parent: CategoryModel | undefined;
-  constructor(item?: Item, parent: CategoryModel) {
-    this._id = item?._id ?? uuidv4();
-    this.parent = parent;
-    if (item?.model) {
-      this.model = observable.object(item?.model);
-    }
-
-    makeAutoObservable(this);
-  }
-
-  setAttribute(attribute: string, value?: any) {
-    if (attribute) {
-      this.model[attribute] = value;
-    }
-  }
-
-  isDate = date => {
-    return (
-      (new Date(date) as unknown as string) != 'Invalid Date' &&
-      !isNaN(new Date(date) as unknown as number)
-    );
-  };
-
-  get titleFieldValue() {
-    return (
-      this.model[this.parent?.titleFieldId as string] ??
-      this.parent?.titleFieldName
-    );
-  }
 }
 
 export class CategoryModel implements ICategory {
---
+++ src/models/ItemModal.ts
@@ -0,0 +1,38 @@
+import { makeAutoObservable, observable } from 'mobx';
+import { v4 as uuidv4 } from 'uuid';
+import { CategoryModel } from './CategoryModel';
+
+export class Item {
+  _id: string = '';
+  model = observable.object({});
+  parent: CategoryModel | undefined;
+  constructor(item: Item, parent: CategoryModel) {
+    this._id = item?._id ?? uuidv4();
+    this.parent = parent;
+    if (item?.model) {
+      this.model = observable.object(item?.model);
+    }
+
+    makeAutoObservable(this);
+  }
+
+  setAttribute(attribute: string, value?: any) {
+    if (attribute) {
+      this.model[attribute] = value;
+    }
+  }
+
+  isDate = date => {
+    return (
+      (new Date(date) as unknown as string) != 'Invalid Date' &&
+      !isNaN(new Date(date) as unknown as number)
+    );
+  };
+
+  get titleFieldValue() {
+    return (
+      this.model[this.parent?.titleFieldId as string] ??
+      this.parent?.titleFieldName
+    );
+  }
+}
