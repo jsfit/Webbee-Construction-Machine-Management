de67dfd02f050ca50b8f17e315f386fb7b06d0d3
MUHAMMAD ILYAS <mail.ilyas@yahoo.com>
2023-03-25 08:44:25 +0500

    modal attribute fix

--- src/models/ItemModal.ts
+++ src/models/ItemModal.ts
@@ -1,17 +1,23 @@
-import { makeAutoObservable, observable } from 'mobx';
+import { makeAutoObservable, observable, autorun, runInAction } from 'mobx';
 import { v4 as uuidv4 } from 'uuid';
 import { CategoryModel } from './CategoryModel';
 
 export class Item {
   _id: string = '';
   model = observable.object({});
-  parent: CategoryModel | undefined;
+  titleFieldId: string | undefined;
+
   constructor(item?: Item, parent?: CategoryModel) {
     this._id = item?._id ?? uuidv4();
-    this.parent = parent;
     if (item?.model) {
       this.model = observable.object(item?.model);
     }
+
+    autorun(() => {
+      runInAction(() => {
+        this.titleFieldId = parent?.titleFieldId;
+      });
+    });
 
     makeAutoObservable(this);
   }
@@ -30,9 +36,6 @@ export class Item {
   };
 
   get titleFieldValue() {
-    return (
-      this.model[this.parent?.titleFieldId as string] ??
-      this.parent?.titleFieldName
-    );
+    return this.model[this.titleFieldId as string] ?? 'UNNAMED FIELD';
   }
 }
